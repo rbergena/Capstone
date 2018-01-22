4d795033250cdd567af5d62d86090c35fe4ba130
Rebecca Bergena <rbergena@gmail.com>
2018-01-22 12:50:52 -0800

    clean comments for background and description files

--- components/UserProfile/Background.js
+++ components/UserProfile/Background.js
@@ -4,19 +4,13 @@ import {
   View,
   Text,
 } from 'react-native';
-import { Tile, Avatar, Button } from 'react-native-elements'
+import { Avatar, Button } from 'react-native-elements'
 
-// functional component to display user background (photo and information (city, state etc.))
-// NOTE: react-native-elements Tile component style raises a warning
-// export function Background(props) {
 export const Background = (props) => {
-  // console.log('these are the props passed down to background')
-  // console.log(props);
   let profilePicture = props.picture;
   let city = props.city;
   let email = props.email;
-  // console.log('this is the profile picture uri')
-  // console.log(profilePicture)
+
   return (
       <View style={styles.container}>
         <Avatar
@@ -37,12 +31,10 @@ export const Background = (props) => {
           {email}
         </Text>
         { props.city ? (
-        <Text style={styles.font}>
-          {city}
-        </Text>) : null
-      }
-
-
+          <Text style={styles.font}>
+            {city}
+          </Text>) : null
+        }
     </View>
   );
 }
@@ -51,71 +43,15 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    // maxHeight: 300,
     alignItems: 'center',
-    // backgroundColor: 'white',
   },
   text: {
     marginTop: 20,
     fontSize: 16,
     color: '#43484d',
-    // fontFamily: 'Helvetica Neue',
-    // flex: 1,
-    // flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center',
   },
   font: {
     fontSize: 12,
     color: '#43484d',
-    // fontFamily: 'Helvetica Neue',
   }
-  // item: {
-  // padding: 10,
-  // fontSize: 18,
-  // height: 70,
-  // },
 });
-
-
-// Stateless functional component way to display a prop using a variable:
-// export const MyComponentClass = (props) => {
-// 	let title = props.title;
-//   return <h1>{title}</h1>;
-// }
-
-
-
-
-// <Tile
-//   imageSrc={{require: ('./img/path')}}
-//   title="Lorem ipsum dolor sit amet, consectetur"
-//   icon={{name: 'play-circle', type: 'font-awesome'}}  // optional
-//   contentContainerStyle={{height: 70}}
-// >
-//   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-//     <Text>Caption</Text>
-//     <Text>Caption</Text>
-//   </View>
-// </Tile>
-
-// <Tile
-//   imageSrc={{uri: profilePicture}}
-//   title="Lorem ipsum dolor sit amet, consectetur"
-//   contentContainerStyle={{height: 70}}
-// >
-//   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-//     <Text>Caption</Text>
-//     <Text>Caption</Text>
-//   </View>
-// </Tile>
-
-// <Tile
-// imageSrc={{ uri: profilePicture}}
-// imageStyle={{resizeMode: 'cover'}}
-// featured
-// title= 'full name'
-// caption= 'email address'
-// />
-// <Button onPress={() => props.navigate('Chat', {props.user})} title="Go to chat" />
-      // <Button onPress={() => props.navigate('Chat', {user: props.user})} title="Go to chat" />
--- components/UserProfile/Description.js
+++ components/UserProfile/Description.js
@@ -11,65 +11,63 @@ import {Icons} from '../../config/InstrumentsGenres';
 
 
 // description component receives user description as props from user profile container
-// export default class Description extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
 export const Description = (props) => {
 
-  // render() {
     return (
-        // <View style={styles}>
         <View style={styles.container}>
-              { props.description ? (
-                <List>
-                <ListItem
-                  title="Description"
-                  titleStyle={styles.title}
-                  hideChevron
-                />
-                <ListItem
-                  subtitle={props.description}
-                  hideChevron
-                />
-                </List>
-              ) : null}
-              { props.instruments ? (
-                <List containerStyle={{borderBottomColor: '#bbb', borderBottomWidth: 1}}>
-                <ListItem
-                  title="Instruments"
-                  titleStyle={styles.title}
-                  hideChevron
-                />
-                <ListItem
-                  containerStyle={{borderBottomWidth: 0}}
-                  subtitle={Object.keys(props.instruments).join(', ')}
-                  hideChevron
-                />
-                <View style={styles.icons}>
-                { props.instruments ? (<Icons instruments={Object.keys(props.instruments)} />)
+          { props.description ? (
+            <List>
+              <ListItem
+                title="Description"
+                titleStyle={styles.title}
+                hideChevron
+              />
+              <ListItem
+                subtitle={props.description}
+                subtitleNumberOfLines={0}
+                subtitleStyle={{ fontSize: 15 }}
+                hideChevron
+              />
+            </List>
+          ) : null}
+          { props.instruments ? (
+            <List containerStyle={{borderBottomColor: '#bbb', borderBottomWidth: 1}}>
+              <ListItem
+                title="Instruments"
+                titleStyle={styles.title}
+                hideChevron
+              />
+              <ListItem
+                containerStyle={{borderBottomWidth: 0}}
+                subtitle={Object.keys(props.instruments).join(', ')}
+                subtitleStyle={{ fontSize: 15 }}
+                hideChevron
+              />
+              <View style={styles.icons}>
+                { props.instruments ? (
+                  <Icons instruments={Object.keys(props.instruments)} />)
                 : null
                 }
-                </View>
-                </List>
-              ) : null}
-              { props.genres ? (
-                <List>
-                <ListItem
-                  title="Genres"
-                  titleStyle={styles.title}
-                  hideChevron
-                />
-                <ListItem
-                  titleStyle={styles.title}
-                  subtitle={Object.keys(props.genres).join(', ')}
-                  hideChevron
-                />
-                </List>
-              ) : null}
+              </View>
+            </List>
+          ) : null}
+          { props.genres ? (
+            <List>
+              <ListItem
+                title="Genres"
+                titleStyle={styles.title}
+                hideChevron
+              />
+              <ListItem
+                titleStyle={styles.title}
+                subtitle={Object.keys(props.genres).join(', ')}
+                subtitleStyle={{ fontSize: 15 }}
+                hideChevron
+              />
+            </List>
+          ) : null}
       </View>
     );
-  // }
 }
 
 const styles = StyleSheet.create({
@@ -80,9 +78,6 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
   },
-  // titleContainer: {
-  //   borderBottomWidth: 3,
-  // }
   icons: {
     flexDirection: 'row',
     marginRight: 10,
@@ -90,35 +85,4 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingLeft: 20,
   },
-
 });
-
-// {isLoggedIn ? (
-//    <LogoutButton onClick={this.handleLogoutClick} />
-//  ) : (
-//    <LoginButton onClick={this.handleLoginClick} />
-//  )}
-//
-// {this.props.links.twitter ? (
-//
-// ) : null}
-// {this.props.links.youtube ? (
-//
-// ): null}
-//
-// { props.instruments ? (
-//   <ListItem
-//     title="Instruments"
-//     titleStyle={styles.title}
-//     subtitle={Object.keys(props.instruments).join(', ')}
-//     hideChevron
-//   />
-// ) : null}
-// { props.genres ? (
-//   <ListItem
-//     title="Genres"
-//     titleStyle={styles.title}
-//     subtitle={Object.keys(props.genres).join(', ')}
-//     hideChevron
-//   />
-// ) : null}

79c21329429d8325997a2d8cad10ed3bdf436de4
Rebecca Bergena <rbergena@gmail.com>
2018-01-22 23:28:30 -0800

    remove extraneous imports and comments

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   StatusBar,
 } from 'react-native';
-import CreateAccountForm from './components/CreateAccount/CreateAccountForm';
 import firebaseApp from './services/firebase';
 import * as firebase from 'firebase';
-import { SignInStack, RootNavigator } from './config/Router.js'
+import { RootNavigator } from './config/Router.js'
 
 console.disableYellowBox = true;
 export default class App extends Component<{}> {
@@ -32,9 +24,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-// <SignInStack />
-
-// <LoginForm/>
-// <View style={styles.container}>
-//   <CreateAccountForm/>
-// </View>
